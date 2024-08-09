d0871300f173298fe1162b06a604bbb0d4c5bbb6
Santiago Crippa <santicrippa@gmail.com>
2024-08-09 19:55:56 -0300

    Populating detail page

--- app/[id].js
+++ app/[id].js
@@ -1,22 +1,55 @@
-import { View, Text } from "react-native";
-import { Link, useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { View, Text, ActivityIndicator, ScrollView, Image } from "react-native";
 
 import Screen from "../components/screen";
+import { Score } from "../components/score";
+import { getGameDetails } from "../lib/getCritics";
 
 export default function GameDetail() {
   const { id } = useLocalSearchParams();
+  const [gameInfo, setGameInfo] = useState(null);
+
+  useEffect(() => {
+    if (id) {
+      getGameDetails(id).then(setGameInfo);
+    }
+  }, [id]);
 
   return (
     <Screen>
-      <View className="flex-1 px-2 justify-center items-center">
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: "#ffee00" },
+          headerTintColor: "black",
+          headerTitle: "Game Detail",
+          headerLeft: () => {},
+          headerRight: () => {},
+        }}
+      />
+      <View className="flex-1 px-2 items-center">
         <View>
-          <Text className="text-white font-bold mb-8 text-2xl">
-            Detalle del juego {id}
-          </Text>
+          {gameInfo === null ? (
+            <ActivityIndicator color={"#fff"} size={"large"} />
+          ) : (
+            <ScrollView>
+              <View className="justify-center items-center text-center">
+                <Image
+                  className="mb-4 rounded-xl"
+                  source={{ uri: gameInfo.img }}
+                  style={{ width: 214, height: 294 }}
+                />
+                <Score score={gameInfo.score} maxScore={100} />
+                <Text className="text-white mt-2 text-center font-bold text-xl">
+                  {gameInfo.title}
+                </Text>
+                <Text className="text-white/70 mt-4 text-left mb-8 text-sm">
+                  {gameInfo.description}
+                </Text>
+              </View>
+            </ScrollView>
+          )}
         </View>
-        <Link href="/" className="text-blue-500">
-          Volver atras
-        </Link>
       </View>
     </Screen>
   );
